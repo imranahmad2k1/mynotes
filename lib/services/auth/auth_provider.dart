a34fb12a592e98e266e3663d87685e1b045e38bb
Imran Ahmad <meetimrang@gmail.com>
2023-04-29 17:12:32 +0500

    step 8: created auth service

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,15 @@
+import 'package:mynotes_practiceproject/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> register({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
